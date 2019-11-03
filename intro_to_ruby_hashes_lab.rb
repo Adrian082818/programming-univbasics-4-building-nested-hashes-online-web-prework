c5bac6f382a38ab453f1237304cb02873edc607f
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-03 19:34:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,11 +37,10 @@ def monopoly_with_third_tier
 	      :four_pieces_owned =>200,
 	    }, 
 	    names: {
-	      :reading_railroad =>{
+	      :reading_railroad =>{}
 	      :key_2 =>"",
 	      :key_3 =>"",
-	      :key_4 =>"",
-},
+	      :key_4 =>"", 
   	 }
   	 }
   	 end
